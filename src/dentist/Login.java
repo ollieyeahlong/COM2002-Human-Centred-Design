5249fea2f0af664af3c00711d7ab0167f5a077bc
User <User@Badger>
2017-11-17 09:00:47 +0000

    Adding the necessary sql commands.

--- src/dentist/Login.java
+++ src/dentist/Login.java
@@ -1,9 +1,18 @@
 package dentist;
 
+import java.awt.*;
+
+import javax.swing.*;
+
 public class Login {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+    	Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+    	
+    	String DB="jdbc:mysql://stusql.dcs.shef.ac.uk/team022?user=dbuser&password=10060fee";
+    	
+    	Conn = DriverManager.getConnection(DB);
+    	
     }
 
 }
