36965435c0be14ec52db85cd5f24461f7461bc65
JE <Work@dyn026233.shef.ac.uk>
2017-11-19 20:06:30 +0000

    login page done

--- src/dentist/Login.java
+++ src/dentist/Login.java
@@ -1,60 +1,150 @@
 package dentist;
 
 import java.awt.*;
-
+import java.awt.event.*;
 import javax.swing.*;
+@SuppressWarnings("unchecked")
 
-public class Login {
+public class Login extends JFrame implements ActionListener {
+
+    JLabel hint;
+
+    public Login() {
+        initUI();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        JComboBox cb = (JComboBox)e.getSource();
+        String userName = (String)cb.getSelectedItem();
+        updateLabel(userName);
+    }
+
+    protected void updateLabel(String name) {
+        hint.setText(name);
+    }
+
+
+    //Make textField get the focus whenever frame is activated.
+    
+    private void initUI() {
+
+        JFrame f = new JFrame("Welcome");
+
+        //JLabel userLabel = new JLabel("Select user");
+
+        JPanel labelPanel = new JPanel(new BorderLayout());
+        labelPanel.setMaximumSize(new Dimension(400, 0));
+        labelPanel.setBorder(BorderFactory.createEmptyBorder(25, 25, 0, 0));
+        // labelPanel.add(userLabel, BorderLayout.NORTH);
+
+
+
+        String[] users = { "Reception", "Other guys"};
+        // -- main show start
+        // make a panel
+        JPanel comboPanel = new JPanel(new BorderLayout());
+        comboPanel.setMaximumSize(new Dimension(400, 0));
+        comboPanel.setBorder(BorderFactory.createEmptyBorder(5, 25, 15, 25));
+        // make a combobox
+        JComboBox userList = new JComboBox(users);
+        userList.setSelectedIndex(0);
+        userList.addActionListener(this);
+        comboPanel.add(userList);
+        
+        // -- main show end
+
+        
+
+        JPanel basic = new JPanel();
+        basic.setLayout(new BoxLayout(basic, BoxLayout.Y_AXIS));
+        add(basic);
+
+        JPanel topPanel = new JPanel(new BorderLayout(0, 0));
+        topPanel.setMaximumSize(new Dimension(750, 0));
+        hint = new JLabel();
+        updateLabel(users[userList.getSelectedIndex()]);
+        hint.setFont (hint.getFont ().deriveFont (50.0f));
+        hint.setBorder(BorderFactory.createEmptyBorder(0, 25, 0, 0));
+        topPanel.add(hint);
+
+        // password enter
+        JPanel passwordPanel = new JPanel(new BorderLayout(0,0));
+        passwordPanel.setMaximumSize(new Dimension(350, 0));
+        JPasswordField passwordField = new JPasswordField(20);
+
+
+        addWindowListener( new WindowAdapter() {
+            public void windowOpened( WindowEvent e ){
+                passwordField.requestFocus();
+            }
+        }); 
+        passwordPanel.add(passwordField, BorderLayout.WEST);
+        JButton b1 = new JButton("Go");
+        b1.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                dispose();
+            }
+        });
+        passwordPanel.add(b1, BorderLayout.EAST);
+        // b1.setHorizontalTextPosition(AbstractButton.LEADING); //aka LEFT, for left-to-right locales
+
+        
+
+
+
+        JSeparator separator = new JSeparator();
+        separator.setForeground(Color.gray);
+
+        topPanel.add(separator, BorderLayout.SOUTH);
+        
+        basic.add(topPanel);
+        basic.add(labelPanel);
+        basic.add(comboPanel);
+        basic.add(passwordPanel);
+        // basic.add(submitButton);
+        
+
+        JPanel boxPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 0));
+        
+        basic.add(boxPanel);
+
+        JPanel bottom = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        
+        JButton quitButton = new JButton("Quit");
+        quitButton.addActionListener((ActionEvent event) -> {
+            System.exit(0);
+        });
+
+        bottom.add(quitButton);
+        basic.add(bottom);
+
+        bottom.setMaximumSize(new Dimension(750, 0));
+
+        setTitle("Welcome");
+        setSize(new Dimension(750, 500));
+        setResizable(false);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+    
 
     public static void main(String[] args) {
-    	// Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+
+        // Class.forName("org.gjt.mm.mysql.Driver").newInstance();
     	
     	// String DB="jdbc:mysql://stusql.dcs.shef.ac.uk/team022?user=dbuser&password=10060fee";
     	
         // Conn = DriverManager.getConnection(DB);
-        new Login();  
-
-    }
-    Login(){  
-        /* JFrame is a top level container (window)
-         * where we would be adding our button
-         */
-        JFrame frame=new JFrame();  
         
+                SwingUtilities.invokeLater(new Runnable() {
         
-                          
-        // Creating Button          
-        JButton b=new JButton("Reception");
-        b.setBackground(Color.RED);
-        b.setOpaque(true);
-        JButton b2=new JButton("Other");
-        b2.setBackground(Color.BLUE);
-        b2.setOpaque(true);
-        /* This method specifies the location and size
-         * of button. In method setBounds(x, y, width, height)
-         * x,y) are cordinates from the top left 
-         * corner and remaining two arguments are the width
-         * and height of the button.
-         */
-
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2); 
-        b.setBounds(50,50,90, 50);  
-        b2.setBounds(150, 50, 90, 50); 
-             
-        //Adding button onto the frame
-        frame.add(b);  
-        frame.add(b2);
-                  
-        // Setting Frame size. This is the window size
-        frame.setSize(300,200);  
-        
-        frame.setLayout(null);  
-        frame.setVisible(true);  
-                  
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-
-        
-                  
-    }  
+                    public void run() {
+                        Login go = new Login();
+                        go.setVisible(true);
+                        
+                    }
+                });
+            }
 }
