677c8eedb1f61e0eb06409cf5346d27b9c45600c
User <User@Badger>
2017-11-24 22:18:40 +0000

    Made Design for Calendar

--- src/dentist/gui/Calendar.java
+++ src/dentist/gui/Calendar.java
@@ -3,18 +3,30 @@ package dentist.gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import java.awt.BorderLayout;
 import javax.swing.JPanel;
-import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 import javax.swing.SwingConstants;
-import java.awt.Font;
 import java.awt.FlowLayout;
-import javax.swing.UIManager;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.BoxLayout;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JSplitPane;
 
 public class Calendar {
 
-	private JFrame frmCalendar;
+	private JFrame frame;
 
 	/**
 	 * Launch the application.
@@ -24,7 +36,7 @@ public class Calendar {
 			public void run() {
 				try {
 					Calendar window = new Calendar();
-					window.frmCalendar.setVisible(true);
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -43,59 +55,108 @@ public class Calendar {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmCalendar = new JFrame();
-		frmCalendar.setResizable(false);
-		frmCalendar.setTitle("Live Calendar Apoint");
-		frmCalendar.setBounds(100, 100, 450, 300);
-		frmCalendar.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmCalendar.getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		JPanel navigationP = new JPanel();
-		navigationP.setBackground(UIManager.getColor("Button.select"));
-		frmCalendar.getContentPane().add(navigationP, BorderLayout.NORTH);
-		navigationP.setLayout(new BorderLayout(0, 0));
-		
-		JButton button_1 = new JButton("<-");
-		navigationP.add(button_1, BorderLayout.WEST);
-		
-		JLabel lblWeekNumber = new JLabel("1st Mon Dec 17");
-		lblWeekNumber.setBackground(UIManager.getColor("Button.select"));
-		lblWeekNumber.setFont(new Font("Lucida Grande", Font.ITALIC, 13));
-		lblWeekNumber.setHorizontalAlignment(SwingConstants.CENTER);
-		navigationP.add(lblWeekNumber, BorderLayout.CENTER);
-		
-		JButton button = new JButton("->");
-		navigationP.add(button, BorderLayout.EAST);
-		
-		JPanel contentP = new JPanel();
-		frmCalendar.getContentPane().add(contentP, BorderLayout.CENTER);
-		contentP.setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel_1 = new JPanel();
-		contentP.add(panel_1, BorderLayout.WEST);
-		
-		JLabel lblNewLabel_1 = new JLabel("New label New label aaa");
-		lblNewLabel_1.setForeground(UIManager.getColor("Button.background"));
-		panel_1.add(lblNewLabel_1);
-		
-		JPanel panel_2 = new JPanel();
-		contentP.add(panel_2, BorderLayout.CENTER);
-		
-		JButton btnNewButton = new JButton("9:30");
-		panel_2.add(btnNewButton);
-		
-		JButton button_2 = new JButton("12:20");
-		panel_2.add(button_2);
-		
-		JButton button_3 = new JButton("15:00");
-		panel_2.add(button_3);
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
-		contentP.add(panel, BorderLayout.EAST);
+		panel.setBackground(Color.LIGHT_GRAY);
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		panel.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblNewLabel = new JLabel("New label New label aaa");
-		lblNewLabel.setForeground(UIManager.getColor("Button.background"));
-		panel.add(lblNewLabel);
+		JButton btnNewButton = new JButton("<-");
+		panel.add(btnNewButton, BorderLayout.WEST);
+		
+		JButton btnNewButton_1 = new JButton("->");
+		panel.add(btnNewButton_1, BorderLayout.EAST);
+		
+		JLabel lblNewLabel = new JLabel("Week");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblNewLabel, BorderLayout.CENTER);
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_panel_1.rowHeights = new int[]{0, 0, 0};
+		gbl_panel_1.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		JLabel lblMonday = new JLabel("Monday");
+		GridBagConstraints gbc_lblMonday = new GridBagConstraints();
+		gbc_lblMonday.insets = new Insets(10, 0, 10, 5);
+		gbc_lblMonday.gridx = 0;
+		gbc_lblMonday.gridy = 0;
+		panel_1.add(lblMonday, gbc_lblMonday);
+		
+		JLabel lblTuesday = new JLabel("Tuesday");
+		GridBagConstraints gbc_lblTuesday = new GridBagConstraints();
+		gbc_lblTuesday.insets = new Insets(10, 0, 10, 5);
+		gbc_lblTuesday.gridx = 1;
+		gbc_lblTuesday.gridy = 0;
+		panel_1.add(lblTuesday, gbc_lblTuesday);
+		
+		JLabel lblWednesday = new JLabel("Wednesday");
+		GridBagConstraints gbc_lblWednesday = new GridBagConstraints();
+		gbc_lblWednesday.insets = new Insets(10, 0, 10, 5);
+		gbc_lblWednesday.gridx = 2;
+		gbc_lblWednesday.gridy = 0;
+		panel_1.add(lblWednesday, gbc_lblWednesday);
+		
+		JLabel lblThursday = new JLabel("Thursday");
+		GridBagConstraints gbc_lblThursday = new GridBagConstraints();
+		gbc_lblThursday.insets = new Insets(10, 0, 10, 5);
+		gbc_lblThursday.gridx = 3;
+		gbc_lblThursday.gridy = 0;
+		panel_1.add(lblThursday, gbc_lblThursday);
+		
+		JLabel lblFriday = new JLabel("Friday");
+		GridBagConstraints gbc_lblFriday = new GridBagConstraints();
+		gbc_lblFriday.insets = new Insets(10, 0, 10, 0);
+		gbc_lblFriday.gridx = 4;
+		gbc_lblFriday.gridy = 0;
+		panel_1.add(lblFriday, gbc_lblFriday);
+		
+		JPanel panel_2 = new JPanel();
+		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
+		gbc_panel_2.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_2.fill = GridBagConstraints.BOTH;
+		gbc_panel_2.gridx = 0;
+		gbc_panel_2.gridy = 1;
+		panel_1.add(panel_2, gbc_panel_2);
+		
+		JPanel panel_3 = new JPanel();
+		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
+		gbc_panel_3.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_3.fill = GridBagConstraints.BOTH;
+		gbc_panel_3.gridx = 1;
+		gbc_panel_3.gridy = 1;
+		panel_1.add(panel_3, gbc_panel_3);
+		
+		JPanel panel_4 = new JPanel();
+		GridBagConstraints gbc_panel_4 = new GridBagConstraints();
+		gbc_panel_4.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_4.fill = GridBagConstraints.BOTH;
+		gbc_panel_4.gridx = 2;
+		gbc_panel_4.gridy = 1;
+		panel_1.add(panel_4, gbc_panel_4);
+		
+		JPanel panel_5 = new JPanel();
+		GridBagConstraints gbc_panel_5 = new GridBagConstraints();
+		gbc_panel_5.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_5.fill = GridBagConstraints.BOTH;
+		gbc_panel_5.gridx = 3;
+		gbc_panel_5.gridy = 1;
+		panel_1.add(panel_5, gbc_panel_5);
+		
+		JPanel panel_6 = new JPanel();
+		GridBagConstraints gbc_panel_6 = new GridBagConstraints();
+		gbc_panel_6.fill = GridBagConstraints.BOTH;
+		gbc_panel_6.gridx = 4;
+		gbc_panel_6.gridy = 1;
+		panel_1.add(panel_6, gbc_panel_6);
 	}
 
 }
